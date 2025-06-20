0e0cda8a9219e3b785c333b4056d52756ea78954
Nattacha <phatcha.06910@gmail.com>
2025-06-20 16:56:28 +0700

    Add main Arduino code
    Initial version of .ino file for LED control using push-botton and INPUT-PULLUP

---
+++ Button-LED-Arduino.ino
@@ -0,0 +1,17 @@
+const int buttonPin = 2;
+const int ledPin = 13;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT_PULLUP); 
+}
+
+void loop() {
+  int buttonState = digitalRead(buttonPin);
+
+  if (buttonState == LOW) {
+    digitalWrite(ledPin, HIGH);
+  } else {
+    digitalWrite(ledPin, LOW);
+  }
+}
